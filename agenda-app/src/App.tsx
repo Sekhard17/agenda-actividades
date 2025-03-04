e290d1d602f40853ef9c123b744c4b808bcd4694
Ignacio Sekhard <joaquin.andrade97@outlook.cl>
2025-03-04 11:05:56 -0300

    implementaciones vercel

--- agenda-app/src/App.tsx
+++ agenda-app/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
 import { Toaster } from 'sonner'
 import Login from './components/Auth/Login'
